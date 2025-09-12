d96c7b3f5c26a83efcf72f0231aaa32a7977715c
Bruno Chiconato <brunochiconato01@gmail.com>
2025-09-12 19:55:35 -0300

    chore(config): set site and base for GitHub Pages deployment
    - Defines site URL and base path for correct routing on GitHub Pages
    - Ensures static output aligns with hosting requirements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://BrunoChiconato.github.io/",
+  base: "/",
+  output: "static",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
@@ -15,7 +17,6 @@ export default defineConfig({
       },
     },
   },
-  output: "static",
   build: {
     inlineStylesheets: "auto",
   },
